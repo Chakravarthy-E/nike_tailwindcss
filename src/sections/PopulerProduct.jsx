f85b84bd749c75c063d4ea5837b84b3c7db4bdec
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2023-08-22 00:43:14 +0530

    project setup

---
+++ src/sections/PopulerProduct.jsx
@@ -0,0 +1,11 @@
+
+
+
+
+const PopulerProduct = () => {
+  return (
+    <div>PopulerProduct</div>
+  )
+}
+
+export default PopulerProduct
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import PopulerProduct from "./PopulerProduct";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+
+export {
+  Hero,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Footer,
+  PopulerProduct,
+  SuperQuality,
+  Subscribe,
+};
